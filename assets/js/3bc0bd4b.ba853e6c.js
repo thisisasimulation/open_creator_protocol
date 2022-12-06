cc72fe959ca27b7cda9a37eff8b66a693c3c7916
arivictor <11803028+arivictor@users.noreply.github.com>
2022-12-06 13:41:01 -0300

    Deploy website - based on 371b162b8f7e9cee545352f7d34ce3c21754cf4f

--- assets/js/3bc0bd4b.ba853e6c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkocp_docs=self.webpackChunkocp_docs||[]).push([[993],{7702:e=>{e.exports=JSON.parse('{"title":"Open Creator Protocol","slug":"/category/open-creator-protocol","permalink":"/category/open-creator-protocol","navigation":{"previous":{"title":"Welcome","permalink":"/"},"next":{"title":"Overview","permalink":"/open-creator-protocol/overview"}}}')}}]);
