3cdd3909ad67965cd43a2e211be41b14465505c0
arivictor <11803028+arivictor@users.noreply.github.com>
2022-12-06 13:39:16 -0300

    Deploy website - based on 371b162b8f7e9cee545352f7d34ce3c21754cf4f

---
+++ assets/js/47921788.b8e2986d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkocp_docs=self.webpackChunkocp_docs||[]).push([[933],{4813:e=>{e.exports=JSON.parse('{"title":"Mint SDK","slug":"/category/mint-sdk","permalink":"/category/mint-sdk","navigation":{"previous":{"title":"Architecture","permalink":"/open-creator-protocol/architecture"},"next":{"title":"Coming Soon","permalink":"/mint-sdk/overview"}}}')}}]);
